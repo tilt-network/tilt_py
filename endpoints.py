69118f2f0a67d75ba7125f927a9e590d0c775b5c
paultergust <prmoreira98@gmail.com>
2025-04-08 19:47:54 -0300

    Add class to check task status via stream

--- endpoints.py
+++ endpoints.py
@@ -11,3 +11,7 @@ def status_polling_endpoint(task_id: str):
 
 def download_processed_data_endpoint(task_id: str):
     return f'{API_BASE_URL}/processed_data/{task_id}'
+
+
+def sse_endpoint(task_id: str):
+    return f'{API_BASE_URL}/sse/{task_id}'
---
+++ task_status_streaming.py
@@ -0,0 +1,48 @@
+import aiohttp
+import asyncio
+from endpoints import sse_endpoint
+from utils import eprint
+
+
+class TaskStatusStreaming:
+    def __init__(self, task_id: str):
+        self.__task_id = task_id
+        self._session: aiohttp.ClientSession | None = None
+        self._task: asyncio.Task | None = None
+        self._stopped = asyncio.Event()
+
+    async def _listen(self):
+        self._session = aiohttp.ClientSession()
+        try:
+            async with self._session.get(sse_endpoint(self.__task_id), headers={"Accept": "text/event-stream"}) as resp:
+                if resp.status != 200:
+                    raise Exception(f"SSE connection failed: {resp.status}")
+
+                async for line in resp.content:
+                    if self._stopped.is_set():
+                        break
+
+                    decoded = line.decode().strip()
+                    if not decoded:
+                        continue  # skip keep-alive or empty lines
+
+                    if decoded.startswith("data:"):
+                        data = decoded.removeprefix("data:").strip()
+                        self.handle_event(data)
+        except Exception as e:
+            eprint(f"SSE error: {e}")
+        finally:
+            await self._session.close()
+
+    def handle_event(self, data: str):
+        print(f"Data status: {data}")
+
+    def start(self):
+        if self._task is None or self._task.done():
+            self._stopped.clear()
+            self._task = asyncio.create_task(self._listen())
+
+    async def stop(self):
+        self._stopped.set()
+        if self._task:
+            await self._task
